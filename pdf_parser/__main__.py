defd7afe2145a429a88bbc3aa984ba42fb7adc51
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-04-12 17:29:14 -0500

    pdf parsing code

---
+++ pdf_parser/__main__.py
@@ -0,0 +1,28 @@
+"""
+This file runs pulls the text from the set of PDFs housed in the
+./data/input folder.
+"""
+import os
+
+from pdf_parser.file_management import write_file_text_to_txt
+from util.pdf import get_file_text, get_files
+
+FOLDER_INPUT = os.getcwd() + "/google_translate_pdfs/data/input/"
+
+
+def main():
+    pdf_files = get_files(FOLDER_INPUT)
+    for file in pdf_files:
+        print(f"Getting text from {file}.")
+        file_text = get_file_text(file, FOLDER_INPUT)
+        print(f"Done getting text from {file}.")
+        filtered_file_text = list(filter(lambda t: t[1] != "", file_text))
+        if len(filtered_file_text) > 0:
+            write_file_text_to_txt(file, file_text)
+            print(f"Done writing text output for {file}.")
+        else:
+            print(f"There was not any parseable text in this file: {file}")
+
+
+if __name__ == "__main__":
+    main()
