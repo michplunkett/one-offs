50ea03f036fcfbccd6a736ea9361d749fa47df91
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-04-28 18:19:32 -0500

    Update census.py

--- geocode_verifier/api/census.py
+++ geocode_verifier/api/census.py
@@ -11,3 +11,19 @@ def get_client():
     """
     key = os.getenv("CENSUS_API_KEY")
     return census.Census(key)
+
+
+def validate_address(address_info, client):
+    """
+    Takes address information and returns validation on that address from the
+    Google Maps API. For documentation, please check this link:
+    https://developers.google.com/maps/documentation/address-validation/requests-validate-address
+
+    :param dictionary address_info: A nested dictionary of strings containing
+        information about a particular address
+    :param googlemaps.Client client: The client that will be used to make the
+        API call.
+
+    :returns The ValidationResult for the query.
+    :rtype googlemaps.ValidationResult
+    """
