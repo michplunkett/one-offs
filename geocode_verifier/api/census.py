0f70377488281bc70e4255166b618ed8aaf1e4c0
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-04-28 18:26:04 -0500

    Update census.py

--- geocode_verifier/api/census.py
+++ geocode_verifier/api/census.py
@@ -1,29 +1,30 @@
 import os
 
-import census
+import censusgeocode as cg
 
 
-def get_client():
-    """
-    Gets the census API client.
-
-    :return: census.Census the Census API client.
-    """
-    key = os.getenv("CENSUS_API_KEY")
-    return census.Census(key)
-
-
-def validate_address(address_info, client):
+def validate_address(address_info):
     """
     Takes address information and returns validation on that address from the
-    Google Maps API. For documentation, please check this link:
-    https://developers.google.com/maps/documentation/address-validation/requests-validate-address
+    Census Geocode API. For documentation, please check this link:
+    https://github.com/fitnr/censusgeocode#census-geocode
 
     :param dictionary address_info: A nested dictionary of strings containing
         information about a particular address
-    :param googlemaps.Client client: The client that will be used to make the
-        API call.
 
     :returns The ValidationResult for the query.
     :rtype googlemaps.ValidationResult
     """
+
+    # Make sure the fields are there.
+    assert bool(address_info["address"])
+    assert bool(address_info["city"])
+    assert bool(address_info["state"])
+    assert bool(address_info["zipcode"])
+
+    return cg.address(
+        address_info["address"],
+        city=address_info["city"],
+        state=address_info["state"],
+        zip=address_info["zipcode"],
+    )
