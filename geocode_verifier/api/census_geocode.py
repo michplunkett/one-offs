c6380a425c8820ec034d60da61f8d84a350f722c
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-04-29 00:34:35 -0500

    Update census_geocode.py

--- geocode_verifier/api/census_geocode.py
+++ geocode_verifier/api/census_geocode.py
@@ -3,13 +3,13 @@ import os
 import censusgeocode as cg
 
 
-def validate_address(address_info):
+def validate_address(info):
     """
     Takes address information and returns validation on that address from the
     Census Geocode API. For documentation, please check this link:
     https://github.com/fitnr/censusgeocode#census-geocode
 
-    :param dictionary address_info: A nested dictionary of strings containing
+    :param dictionary info: A nested dictionary of strings containing
         information about a particular address
 
     :returns The ValidationResult for the query.
@@ -17,14 +17,14 @@ def validate_address(address_info):
     """
 
     # Make sure the fields are there.
-    assert bool(address_info["address"])
-    assert bool(address_info["city"])
-    assert bool(address_info["state"])
-    assert bool(address_info["zipcode"])
+    assert bool(info["address"])
+    assert bool(info["city"])
+    assert bool(info["state"])
+    assert bool(info["zipcode"])
 
     return cg.address(
-        address_info["address"],
-        city=address_info["city"],
-        state=address_info["state"],
-        zip=address_info["zipcode"],
+        info["address"],
+        city=info["city"],
+        state=info["state"],
+        zip=info["zipcode"],
     )
