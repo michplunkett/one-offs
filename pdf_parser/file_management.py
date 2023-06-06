f4eb4971040c1e1b0dc0eaf0b0ce3eea8e6b9d91
Michael Plunkett <5885605+michplunkett@users.noreply.github.com>
2023-06-06 15:29:51 -0500

    Add `small_task` Jupyter notebook (#17)
    ## What does this pull request add?
    It tracks the changes made on the `small_task` Jupyter notebook and
    created a write to JSON function in the `util` package.
    
    ## How was it tested?
    - [x] Manually tested code.
    - [x] I ran `make lint` and have made the bulk of changes that it has
    requested.

--- pdf_parser/file_management.py
+++ pdf_parser/file_management.py
@@ -1,6 +1,10 @@
 import os
 
-from util.constants import ENCODING_STANDARD, EXTENSION_TXT, FILE_OPEN_WRITE
+from util.constants import (
+    ENCODING_STANDARD,
+    EXTENSION_TXT,
+    FILE_OPEN_MODE_WRITE,
+)
 
 FOLDER_OUTPUT = os.getcwd() + "/pdf_parser/data/output/"
 
@@ -9,7 +13,7 @@ def write_file_text_to_txt(original_file_name, text):
     name, _ = os.path.splitext(original_file_name)
     with open(
         FOLDER_OUTPUT + name + EXTENSION_TXT,
-        FILE_OPEN_WRITE,
+        FILE_OPEN_MODE_WRITE,
         encoding=ENCODING_STANDARD,
     ) as text_file:
         text_file.writelines(text)
--- util/files.py
+++ util/files.py
@@ -2,8 +2,13 @@
 This file is a general file utility.
 """
 import csv
+import json
 
-from util.constants import ENCODING_STANDARD
+from util.constants import (
+    ENCODING_STANDARD,
+    FILE_OPEN_MODE_READ,
+    FILE_OPEN_MODE_WRITE,
+)
 
 
 def read_csv_to_dict(file_path):
@@ -17,9 +22,28 @@ def read_csv_to_dict(file_path):
     """
     rows = []
 
-    with open(file_path, newline="", encoding=ENCODING_STANDARD) as csvfile:
-        reader = csv.DictReader(csvfile)
+    with open(
+        file_path,
+        encoding=ENCODING_STANDARD,
+        mode=FILE_OPEN_MODE_READ,
+        newline="",
+    ) as csv_file:
+        reader = csv.DictReader(csv_file)
         for r in reader:
             rows.append(r)
 
     return rows
+
+
+def write_to_json(file_path, json_dict):
+    """
+    This function writes a dictionary to a JSON file.
+
+    :param str file_path: The path of the desired file.
+    :param dict json_dict: A dictionary meant to be written as JSON.
+    """
+
+    with open(
+        file_path, encoding=ENCODING_STANDARD, mode=FILE_OPEN_MODE_WRITE
+    ) as out_file:
+        json.dump(json_dict, out_file, indent=4)
