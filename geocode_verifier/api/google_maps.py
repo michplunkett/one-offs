1b7354e620ef0ff4b4d86102e0327765c5f53d46
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-07-15 00:12:15 -0500

    Update google_maps.py

--- geocode_verifier/api/google_maps.py
+++ geocode_verifier/api/google_maps.py
@@ -1,14 +1,11 @@
+"""GoogleMaps API functions."""
 import os
 
 import googlemaps
 
 
 def get_client():
-    """
-    Gets the GoogleMaps API client.
-
-    :return: googlemaps.Client
-    """
+    """Get the GoogleMaps API client."""
     key = os.getenv("GOOGLE_MAPS_API_KEY")
 
     # Make sure there is an API key.
@@ -19,16 +16,10 @@ def get_client():
 
 def validate_address(client, info):
     """
-    Takes address information and returns validation on that address from the
-    Google Maps API. For documentation, please check this link:
+    Takes address information and returns validation on that address from the Google Maps API.
+
+    For documentation, please check this link:
     https://developers.google.com/maps/documentation/address-validation/requests-validate-address.
-
-    :param client: the GoogleMaps API client
-    :param dictionary info: A nested dictionary of strings containing
-        information about a particular address
-
-    :returns The ValidationResult for the query.
-    :rtype googlemaps.ValidationResult
     """
     # Assert that the necessary fields are there.
     assert bool(info)
